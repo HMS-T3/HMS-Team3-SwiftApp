5fb167f1cc31ae1f230a08ea4705158efcd2a1f3
Niya <sniya359@gmail.com>
2023-04-10 15:04:21 +0530

    Initial Commit

---
+++ HMS-Team3-SwiftApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HMS-Team3-SwiftApp
+//
+//  Created by Niya Shameer on 10/04/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ HMS-Team3-SwiftAppUITests/HMS_Team3_SwiftAppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  HMS_Team3_SwiftAppUITestsLaunchTests.swift
+//  HMS-Team3-SwiftAppUITests
+//
+//  Created by Niya Shameer on 10/04/23.
+//
+
+import XCTest
+
+final class HMS_Team3_SwiftAppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
