d4bf34b1925b95387494eafd980f025103759da4
Aryan Singh <74867817+aryansingh920@users.noreply.github.com>
2023-04-19 15:09:58 +0530

    basic Get and Post routes done (#9)

---
+++ HMS-Team3-SwiftApp/API/API.swift
@@ -0,0 +1,15 @@
+//
+//  API.swift
+//  HMS-Team3-SwiftApp
+//
+//  Created by Aryan Singh on 19/04/23.
+//
+
+import Foundation
+
+class API{
+    init(){
+        let MethodInstance = Methods(baseURI: "https://hmst3-backend.onrender.com/app")
+//        print("MethodInstance.GET(route:)",MethodInstance.GET(route: "/"))
+    }
+}
---
+++ HMS-Team3-SwiftApp/API/Methods.swift
@@ -0,0 +1,40 @@
+//
+//  APICalls.swift
+//  HMS-Team3-SwiftApp
+//
+//  Created by Aryan Singh on 19/04/23.
+//
+
+import Foundation
+import Alamofire
+
+class Methods{
+    let baseURI:String
+    init(baseURI:String){
+        self.baseURI = baseURI
+    }
+    func GET(route:String){
+        AF.request("\(baseURI)\(route)", method: .get)
+            .validate()
+            .responseJSON { response in
+                switch response.result {
+                case .success(let value):
+                    print("JSON: \(value)")
+                case .failure(let error):
+                    print(error)
+                }
+            }
+    }
+    func POST(route:String,parameters:[String:String]){
+        AF.request("\(baseURI)\(route)", method: .post, parameters: parameters)
+            .validate()
+            .responseJSON { response in
+                switch response.result {
+                case .success(let value):
+                    print("JSON: \(value)")
+                case .failure(let error):
+                    print(error)
+                }
+            }
+    }
+}
