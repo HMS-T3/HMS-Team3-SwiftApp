b64126fee814a6664c426e19b84082da3fc79401
Devjyoti Mohanty <68328634+Dev26112001@users.noreply.github.com>
2023-04-24 20:20:32 +0530

    Medical History (#14)
    * Onboarding
    `
    
    * commitChanges
    
    * stash
    
    * merged conflicts
    
    ---------
    
    Co-authored-by: aryansingh920 <aryansingh920@outlook.com>

---
+++ HMS-Team3-SwiftApp/Storybords/PatientStoryboards/Medical History Data Model/MedicalhistoryDataModel.swift
@@ -0,0 +1,23 @@
+//
+//  MedicalhistoryDataModel.swift
+//  HMS-Team3-SwiftApp
+//
+//  Created by Devjyoti Mohanty on 24/04/23.
+//
+
+import Foundation
+import UIKit
+
+struct Cancer {
+    let name: String?
+    let img: UIImage?
+    let title: String?
+}
+
+struct Migraine {
+    let name: String?
+    let img: UIImage?
+    let title: String?
+}
+
+
