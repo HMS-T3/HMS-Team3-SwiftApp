f067112a89184a1451e39721c2bf559a309f10b0
AkshatNaruka <narukaakshat@gmail.com>
2023-04-24 17:34:46 +0530

    HMST3-305 Implementation of SOS contacts

--- HMS-Team3-SwiftApp/Controllers/Profile Sub-Screens/SOSContactsViewController.swift
+++ HMS-Team3-SwiftApp/Controllers/Profile Sub-Screens/SOSContactsViewController.swift
@@ -8,7 +8,9 @@
 import UIKit
 
 class SOSContactsViewController: UIViewController {
-
+    
+    
+    @IBOutlet var contactsCardCollectionView: UICollectionView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -16,4 +18,24 @@ class SOSContactsViewController: UIViewController {
 		view.backgroundColor = .systemBackground
     }
 
+}
+
+extension SOSContactsViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        
+
+        cell.layer.borderWidth = 2
+//        cell.layer.borderColor = UIColor.gray.cgColor
+        cell.layer.cornerRadius = 18
+//        cell.backgroundColor = UIColor."#199A8E"
+    
+        return cell
+    }
+    
+    
 }
