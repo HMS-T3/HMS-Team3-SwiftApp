967c53eb9604b8c7f69a0cb016556aec2bac1021
aryansingh920 <aryansingh920@outlook.com>
2023-04-25 13:35:35 +0530

    merged

---
+++ HMS-Team3-SwiftApp/Models/LoginPatient.swift
@@ -0,0 +1,24 @@
+//
+//  LoginPatient.swift
+//  HMS-Team3-SwiftApp
+//
+//  Created by Shivacharan Reddy on 24/04/23.
+//
+
+import Foundation
+
+struct LoginPatient: Codable {
+	let response: Response?
+	let status: Bool
+	
+	enum CodingKeys: String, CodingKey {
+		case response = "Response"
+		case status = "Status"
+	}
+}
+
+struct Response: Codable {
+	
+	let id: String
+	let Message: String
+}
