e4f20f40bdaef7766aeac01d2a4466f069396258
aryansingh920 <aryansingh920@outlook.com>
2023-05-02 10:38:02 +0530

    patient profile (#23)

---
+++ HMS-Team3-SwiftApp/Controllers/Storyboards/DoctorStoryboards/DoctorSchedulePatientAppointmentViewController.swift
@@ -0,0 +1,19 @@
+//
+//  DoctorSchedulePatientAppointmentViewController.swift
+//  HMS-Team3-SwiftApp
+//
+//  Created by Ujjwalsingh Rajput on 01/05/23.
+//
+
+import UIKit
+
+class DoctorSchedulePatientAppointmentViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+
+}
